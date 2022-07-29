7d4e7ee006efd88c7c178c076050f3fe45c189ef
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2022-07-29 00:40:11 -0700

    Update test_changelog_and_version.py

--- tests/test_changelog_and_version.py
+++ tests/test_changelog_and_version.py
@@ -1,7 +1,7 @@
 """Tests for flake8-trio package metadata."""
 import re
 from pathlib import Path
-from typing import NamedTuple, Optional
+from typing import Iterable, NamedTuple
 
 import flake8_trio
 
@@ -9,24 +9,37 @@ import flake8_trio
 class Version(NamedTuple):
     year: int
     month: int
-    day: int
+    patch: int
 
     @classmethod
     def from_string(cls, string):
         return cls(*map(int, string.split(".")))
 
 
-def get_latest_release() -> Optional[Version]:
+def get_releases() -> Iterable[Version]:
     valid_pattern = re.compile(r"^## (\d\d\.\d?\d\.\d?\d)$")
     with open(Path(__file__).parent.parent / "CHANGELOG.md") as f:
-        for aline in f.readlines():
-            version_match = valid_pattern.match(aline)
-            if version_match:
-                return Version.from_string(version_match.group(1))
-
-    return None
+        lines = f.readlines()
+    for aline in lines:
+        version_match = valid_pattern.match(aline)
+        if version_match:
+            yield Version.from_string(version_match.group(1))
 
 
 def test_last_release_against_changelog():
     """Ensure we have the latest version covered in CHANGELOG.md"""
-    assert get_latest_release() == Version.from_string(flake8_trio.__version__)
+    latest_release, *_ = get_releases()
+    assert latest_release == Version.from_string(flake8_trio.__version__)
+
+
+def test_version_increments_are_correct():
+    versions = list(get_releases())
+    for prev, current in zip(versions[1:], versions):
+        assert prev < current  # remember that `versions` is newest-first
+        msg = f"{current=} does not follow {prev=}"
+        # for CalVer, we either increment the patch version by one, or
+        # increment the time-based parts and set the patch version to one.
+        if current.patch == 1:
+            assert prev[:2] < current[:2], msg
+        else:
+            assert current == prev._replace(patch=prev.patch + 1), msg
