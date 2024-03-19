6c80844836ef25fbff395eebb25b83fa73ea3f06
jakkdl <h6+github@pm.me>
2024-03-19 13:09:21 +0100

    bump __version__

--- flake8_async/__init__.py
+++ flake8_async/__init__.py
@@ -37,7 +37,7 @@ if TYPE_CHECKING:
 
 
 # CalVer: YY.month.patch, e.g. first release of July 2022 == "22.7.1"
-__version__ = "24.3.3"
+__version__ = "24.3.4"
 
 
 # taken from https://github.com/Zac-HD/shed
