3021de5b3ef91fe9f5dc43a87c9191a95294adb5
jakkdl <h6+github@pm.me>
2024-05-27 13:14:26 +0200

    add testing of from contextlib import *

---
+++ tests/autofix_files/exception_suppress_context_manager_import_star.py
@@ -0,0 +1,16 @@
+# ARG --enable=ASYNC910
+# see exception_suppress_context_manager.py for main testing
+
+# AUTOFIX
+# ASYNCIO_NO_AUTOFIX
+
+from contextlib import *
+
+# NOTE: remove in #255
+import trio
+
+
+async def foo():  # ASYNC910: 0, "exit", Statement('function definition', lineno)
+    with suppress():
+        await foo()
+    await trio.lowlevel.checkpoint()
